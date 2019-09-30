7b17b45fc62ffd71f522f53333c71a6ce0b7cff6
Nathan Bohn <nathantbohn@gmail.com>
2019-09-30 00:39:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,8 +33,8 @@ end
 def using_delete(array, string)
   array.delete(string)
 end 
-def using_delete_at(array, integer)
-  array.delete(integer)
+def using_delete_at(array, element)
+  array.delete(element)
 end 
 
   
