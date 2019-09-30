ed5fcf6b49ff57aa91ab56f7c87fc81163d9f41b
Nathan Bohn <nathantbohn@gmail.com>
2019-09-30 00:42:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,15 +8,13 @@ def using_pop(array)
   return array.pop 
 end
 def pop_with_args(array)
-  secL = array.pop 
-  l = array.pop 
-  return secL && l 
+    array.pop(2)
 end 
 def using_shift(array)
   return array.shift 
 end 
 def shift_with_args(array)
-  return array.shift
+  return array.shift(2)
 end 
 def using_concat(array1,array2)
   array1.concat(array2)
