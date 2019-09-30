a5158b3d8c002d1bd039929e768f079a6b9aca8c
Nathan Bohn <nathantbohn@gmail.com>
2019-09-30 00:36:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,22 @@ def using_concat(array1,array2)
   array1.concat(array2)
 end
 def using_insert(array, element)
-  array.insert()
+  array.insert(3, element)
+end 
+def using_uniq(array)
+  array.uniq 
+end 
+def using_flatten(array)
+  return array.flatten 
+end 
+def using_delete(array, string)
+  array.delete(string)
+end 
+def using_delete_at(array, integer)
+  array.delete(integer)
+end 
+
+  
+
   
   
